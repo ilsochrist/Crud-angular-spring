0bfbfc54149845e52f720fd7b03716bd3c12ca59
Ilso <ilso_1000@hotmail.com>
2022-12-13 20:27:48 -0300

    first commit

---
+++ crud-spring/src/main/java/com/ilso/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.ilso.repository;
+
+import com.ilso.model.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
